c4d6682395c9d1ca6ff3480e302abe450cbb6bee
hzkkong <hz0227798@gmail.com>
2024-10-12 22:09:54 +0900

    그림판 Position 출력
    근데 왜 씬에서 안보이는데......

---
+++ Assets/Hz/Scripts/K_LineRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_LineRenderer : MonoBehaviour
+{
+
+    private LineRenderer line;
+    private Vector3 prevPos;
+
+    [SerializeField]
+    private float minDistance = 0.1f;
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 0;
+
+        mainCamera = Camera.main;
+        prevPos = transform.position;
+
+    }
+
+    void Update()
+    {
+
+        // 마우스로 먼저
+        if(Input.GetMouseButton(0))
+        {
+            // 마우스 스크린 좌표를 캔버스 로컬 좌표로 변환
+            Vector3 currPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            currPos.z = -9; //mainCamera.nearClipPlane;
+
+            if(Vector3.Distance(currPos, prevPos) > minDistance)
+            {
+                print("제발그려져");
+                line.positionCount++;
+                line.SetPosition(line.positionCount - 1, currPos);
+                prevPos = currPos;
+            }
+            else
+            {
+                prevPos = Vector3.zero;
+            }
+        }
+    }
+}
