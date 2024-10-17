ab42b6c7d4ed6bc8aad6c645400941b0af6383d0
hzkkong <hz0227798@gmail.com>
2024-10-17 18:27:29 +0900

    집갈거야

--- Assets/Hz/Scripts/K_PaintManager.cs
+++ Assets/Hz/Scripts/K_PaintManager.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class K_PaintManager : MonoBehaviour
 {
-    public K_DrawPen drawTool;
-    public K_Eraser eraserTool;
+    private K_DrawPen drawTool;
+    private K_Eraser eraserTool;
+
+    // 버튼
+    //public Button btn_Color;
+    public Button btn_Eraser;
+    public Button btn_Create;
 
     // enum
     public ToolType toolType = ToolType.Pen;
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
             SetPenMode();
         }
 
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             SetEraserMode();
         }
@@ -31,5 +37,12 @@ public class K_PaintManager : MonoBehaviour
     public void SetEraserMode()
     {
         toolType = ToolType.Eraser;
+        
+    }
+
+    public void CreateCharacter()
+    {
+        // 만들기 전 비활성
+        // 버튼 누르면 캐릭터 생성
     }
 }
