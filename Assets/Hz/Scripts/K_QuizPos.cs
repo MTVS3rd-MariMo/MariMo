2e4a9bdae76b6bf2c418d21d1b7c187e7695209e
hzkkong <hz0227798@gmail.com>
2024-10-18 21:30:39 +0900

    퀴즈 셋팅 - 1차

---
+++ Assets/Hz/Scripts/K_QuizPos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_QuizPos : MonoBehaviour
+{
+    public GameObject quiz1;
+    public GameObject quiz2;
+
+    void Start()
+    {
+        // 처음에는 둘다 비활성화
+        quiz1.gameObject.SetActive(false);
+        quiz2.gameObject.SetActive(false);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거된 오브젝트: " + other.gameObject.name);
+
+        if (other.CompareTag("Player"))
+        {
+            print("플레이어다");
+            //quiz1.gameObject.SetActive(true);
+            quiz2.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
---
+++ Assets/Hz/Scripts/K_QuizPos_2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_QuizPos_2 : MonoBehaviour
+{
+    public GameObject quiz2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quiz2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거된 오브젝트: " + other.gameObject.name);
+
+        if (other.CompareTag("Player"))
+        {
+            print("플레이어다");
+            //quiz1.gameObject.SetActive(true);
+            quiz2.gameObject.SetActive(true);
+        }
+    }
+}
