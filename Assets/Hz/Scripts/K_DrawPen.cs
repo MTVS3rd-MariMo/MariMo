7b03e5019625f0220bd9ba461f32aec18c91f599
hzkkong <khj0227798@gmail.com>
2024-10-18 14:48:30 +0900

    그림판 기본 기능 1차 완료
    원툴, 색상 변경(5개), 지우개

--- Assets/Hz/Scripts/K_DrawPen.cs
+++ Assets/Hz/Scripts/K_DrawPen.cs
@@ -10,6 +10,12 @@ public class K_DrawPen : MonoBehaviour
 
     public LineRenderer line;
     public Material penMaterial;
+    // 기본 색상은 검정
+    private Color currColor = Color.black;
+    // 현재 머터리얼
+    private Material currPenMaterial;
+    // 이전에 그린 선 저장 리스트
+    private List<LineRenderer> drawLines = new List<LineRenderer>();
 
     private bool isDrawing = false;
 
@@ -22,7 +28,7 @@ public class K_DrawPen : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             StartDrawing();
         }
@@ -41,14 +47,22 @@ public class K_DrawPen : MonoBehaviour
     public void StartDrawing()
     {
         // 스케치북 안에서만 활성화되도록 -> 라인렌더러 생성 막기
-        if(RectTransformUtility.RectangleContainsScreenPoint(sktechBook, Input.mousePosition, uiCamera))
+        if (RectTransformUtility.RectangleContainsScreenPoint(sktechBook, Input.mousePosition, uiCamera))
         {
             isDrawing = true;
             line = new GameObject("K_Crayon").AddComponent<LineRenderer>();
-
             // 생성된 GameObject의 위치를 rt 위치로 설정 (또는 원하는 위치로 설정)
             line.transform.position = rt.transform.position;
-            line.material = penMaterial;
+            //line.material = penMaterial;
+
+            // 현재 선 리스트에 추가
+            drawLines.Add(line);
+
+            // 추가
+            currPenMaterial = new Material(Shader.Find("Sprites/Default"));
+            currPenMaterial.color = currColor;
+
+            line.material = currPenMaterial;
             line.positionCount = 0;
             line.useWorldSpace = true;
             line.gameObject.layer = LayerMask.NameToLayer("UI");
@@ -56,13 +70,17 @@ public class K_DrawPen : MonoBehaviour
             line.sortingOrder = 1;
             line.startWidth = 0.5f;
             line.endWidth = 0.5f;
+            line.gameObject.AddComponent<BoxCollider>();
+
+            // 색상 설정
+            line.startColor = currColor;
+            line.endColor = currColor;
         }
-        
     }
 
     public void UpdateDrawing(Vector3 startPos)
     {
-        if(isDrawing)
+        if (isDrawing)
         {
             Vector2 mousePos;
             // 마우스 좌표 rt의 로컬로 변환
@@ -75,7 +93,7 @@ public class K_DrawPen : MonoBehaviour
             worldPos.z = zPosition;
 
             // 스케치북 안에 있을 시 그려지게
-            if(RectTransformUtility.RectangleContainsScreenPoint(sktechBook, Input.mousePosition, uiCamera))
+            if (RectTransformUtility.RectangleContainsScreenPoint(sktechBook, Input.mousePosition, uiCamera))
             {
                 line.positionCount++;
                 line.SetPosition(line.positionCount - 1, worldPos);
@@ -91,13 +109,30 @@ public class K_DrawPen : MonoBehaviour
                 // 해당영역 벗어나면 LineRenderer 생성안되게 함
                 //StopDrawing();
             }
-            
+
         }
     }
 
     public void StopDrawing()
     {
         isDrawing = false;
+    }
+
+    // 색상 변경
+    public void SetPenColor(Color color)
+    {
+        currColor = color;
+        //penMaterial.color = currColor;
+        // 현재 그리고 있는 선에만 적용 추가
+        if(isDrawing && line != null)
+        {
+            line.startColor = currColor;
+            line.endColor = currColor;
+            if(currPenMaterial != null)
+            {
+                currPenMaterial.color = currColor;
+            }
+        }
     }
 
 }
