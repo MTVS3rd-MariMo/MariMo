7b2abcb62f7e192f827c01750126d9254645ecd2
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-10-20 12:41:12 +0900

    hot seating ui

--- Assets/YDW/Scripts/Y_HotSeatController.cs
+++ Assets/YDW/Scripts/Y_HotSeatController.cs
@@ -1,18 +1,56 @@
+﻿using Cinemachine.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Y_HotSeatController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject guideImg;
+
+    //public InputField selfIntroduce;
+    public RectTransform inputFieldRect;
+    public Vector2 expandedSize = new Vector2(1200, 200); // 확장된 크기
+    public Vector2 expandedPos = new Vector2(-345, 180); // 확장됐을 때 위치
+    private Vector2 originalSize;
+    private Vector2 originalPosition;
+    private TouchScreenKeyboard keyboard;
+
     void Start()
     {
-        
+        originalSize = inputFieldRect.sizeDelta;
+        originalPosition = inputFieldRect.gameObject.transform.localPosition;
+        StartCoroutine(Deactivate());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Deactivate()
     {
-        
+        yield return new WaitForSeconds(2);
+        guideImg.SetActive(false);
+    }
+
+    // InputField가 선택되었을 때 호출
+    public void OnSelect(BaseEventData eventData)
+    {
+        inputFieldRect.sizeDelta = expandedSize;
+        inputFieldRect.gameObject.transform.localPosition = expandedPos;
+
+        // 터치 키보드 호출 (모바일에서만 동작)
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+    }
+
+    // InputField가 선택 해제되었을 때 호출
+    public void OnDeselect(BaseEventData eventData)
+    {
+        inputFieldRect.sizeDelta = originalSize;
+        inputFieldRect.gameObject.transform.localPosition = originalPosition;
+
+        // 터치 키보드 닫기
+        if (keyboard != null && keyboard.active)
+        {
+            keyboard.active = false;
+        }
     }
 }
