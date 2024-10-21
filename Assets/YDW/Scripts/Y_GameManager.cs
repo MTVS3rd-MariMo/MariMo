388b6bc96df2f149cb8f1c00ffbeb73ce44a66d2
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-10-21 16:05:08 +0900

    Photon 이동 동기화 시작

--- Assets/YDW/Scripts/Y_GameManager.cs
+++ Assets/YDW/Scripts/Y_GameManager.cs
@@ -7,7 +7,13 @@ public class Y_GameManager : MonoBehaviour
 {
     void Start()
     {
+        print("!!!!!!!!!!!!!");
         StartCoroutine(SpawnPlayer());
+
+        // OnPhotonSerializeView 에서 데이터 전송 빈도 수 설정하기 (per seconds)
+        PhotonNetwork.SerializationRate = 30;
+        // 대부분의 데이터 전송 빈도 수 설정하기 (per seconds)
+        PhotonNetwork.SendRate = 30;
     }
 
     IEnumerator SpawnPlayer()
@@ -18,6 +24,7 @@ public class Y_GameManager : MonoBehaviour
         Vector2 randomPos = Random.insideUnitCircle * 5.0f;
         Vector3 initPosition = new Vector3(randomPos.x, 1.0f, randomPos.y);
 
+        print("??????????????????");
         PhotonNetwork.Instantiate("Player", initPosition, Quaternion.identity);
     }
 
