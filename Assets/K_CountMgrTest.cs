48eb9b3e39bbb1ebf638b3e83ba9c593a57642ce
hzkkong <hz0227798@gmail.com>
2024-10-26 18:27:06 +0900

    임시 커밋3

---
+++ Assets/K_CountMgrTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_CountMgrTest : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        // 4명 이상 들어오고 LobbyUiManager 있으면 실행시켜
+        if (players.Length >= 4 && K_LobbyUiManager.instance != null)
+        {
+            K_LobbyUiManager.instance.isAllArrived = true;
+            print("4명 다옴");
+        }
+        else if (K_LobbyUiManager.instance != null)
+        {
+            K_LobbyUiManager.instance.isAllArrived = false;
+            print("아직 4명 안댐");
+        }
+    }
+}
