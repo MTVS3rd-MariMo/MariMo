59e903cbc81887ec98599fad61189eaa9c60dfbf
hzkkong <hz0227798@gmail.com>
2024-11-03 22:49:26 +0900

    북캔버스 재설정 및 아바타 통신 (아직 안끝남)

--- Assets/Hz/Scripts/Server/K_HttpAvatar.cs
+++ Assets/Hz/Scripts/Server/K_HttpAvatar.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -15,25 +16,24 @@ public class K_HttpAvatar : MonoBehaviour
     // 아바타 이미지 받기
     public GameObject ChooseCharacterUI;
     public Image avatarImage;
+    public GameObject btn_MakeAvatar;
+    public GameObject btn_ToMap;
 
     // URL
-    public string uploadUrl = "http://172.30.1.44:8080/api/avatar/upload-img";
-    public string downloadUrl = "";
+    public string uploadUrl = "http://211.250.74.75:8899/api/avatar/upload-img";
 
     void Start()
     {
-        btn_CreateAvatar.onClick.AddListener(CreateAvatar);
+        //btn_CreateAvatar.onClick.AddListener(CreateAvatar);
     }
-    
-    // 서버에 아바타 보내기
+
     public IEnumerator UploadTextureAsPng()
     {
         Texture2D textureToUpload = rawImage.texture as Texture2D;
 
         if (textureToUpload == null)
         {
-            Debug.Log("텍스쳐 설정 안댐 ");
-
+            Debug.LogError("텍스처가 설정되지 않았습니다.");
             yield break;
         }
 
@@ -42,28 +42,94 @@ public class K_HttpAvatar : MonoBehaviour
         HttpInfo info = new HttpInfo
         {
             url = uploadUrl,
-            body = "img",
             contentType = "multipart/form-data",
+            body = "img",
             onComplete = (DownloadHandler downloadHandler) =>
             {
-                // 응답 받아
-                Debug.Log("Upload 완료 : " + downloadHandler.text);
 
-                // 업로드 완료됐다면, PaintUI 비활성화, ChooseCharacterUI 활성화 시키기
+                // 서버 응답을 JSON으로 파싱하여 이미지 처리
+                UserAvatarData avatarData = JsonUtility.FromJson<UserAvatarData>(downloadHandler.text);
+
+                // Base64 인코딩된 avatarImg 데이터를 디코딩하여 Texture2D로 변환
+                byte[] imageData = Convert.FromBase64String(avatarData.avatarImg);
+                Texture2D receivedTexture = new Texture2D(2, 2);
+                bool isLoaded = receivedTexture.LoadImage(imageData);
+
+                if (isLoaded)
+                {
+                    // Texture2D를 Sprite로 변환하여 avatarImage에 적용
+                    Sprite receivedSprite = Sprite.Create(
+                        receivedTexture,
+                        new Rect(0, 0, receivedTexture.width, receivedTexture.height),
+                        new Vector2(0.5f, 0.5f)
+                    );
+                    avatarImage.sprite = receivedSprite;
+
+                    Debug.Log("아바타 이미지가 성공적으로 UI에 적용되었습니다.");
+                }
+                else
+                {
+                    Debug.LogError("이미지 로딩 실패");
+                }
+
+                // UI 상태 변경
                 PaintUI.SetActive(false);
                 ChooseCharacterUI.SetActive(true);
-
-                // 서버에서 전송해준 이미지 다운로드해서 표시해주기
-                StartCoroutine(OnDownloadImage());
+                btn_MakeAvatar.SetActive(false);
+                btn_ToMap.SetActive(true);
             }
         };
 
-        // 디버그 체크
-        Debug.Log("Attempting to upload to URL: " + info.url);
-        Debug.Log("Coroutine Started");
-
-        Debug.Log("이미지 데이터 길이 " + pngData.Length);
         yield return StartCoroutine(HttpManager.GetInstance().UploadFileByFormDataArt(info, pngData));
+
+
+
+        //Texture2D textureToUpload = rawImage.texture as Texture2D;
+
+        //if (textureToUpload == null)
+        //{
+        //    Debug.Log("텍스쳐 설정 안댐 ");
+
+        //    yield break;
+        //}
+
+        //byte[] pngData = textureToUpload.EncodeToPNG();
+
+        //HttpInfo info = new HttpInfo
+        //{
+        //    url = uploadUrl,
+        //    contentType = "multipart/form-data",
+        //    body = "img",
+        //    onComplete = (DownloadHandler downloadHandler) =>
+        //    {
+        //        // 응답으로 받은 이미지 데이터를 Texture2D로 변환
+        //        Texture2D receivedTexture = new Texture2D(2, 2);
+        //        receivedTexture.LoadImage(downloadHandler.data);
+
+        //        print(downloadHandler.data);
+
+        //        // Texture2D를 Sprite로 변환하여 avatarImage에 적용
+        //        Sprite receivedSprite = Sprite.Create(
+        //            receivedTexture,
+        //            new Rect(0, 0, receivedTexture.width, receivedTexture.height),
+        //            new Vector2(0.5f, 0.5f)
+        //        );
+        //        avatarImage.sprite = receivedSprite;
+
+        //        print("잘 되는거니..");
+
+        //        // PaintUI 비활성화 및 ChooseCharacterUI 활성화
+        //        PaintUI.SetActive(false);
+        //        ChooseCharacterUI.SetActive(true);
+        //        btn_MakeAvatar.SetActive(false);
+        //        btn_ToMap.SetActive(true);
+
+        //        Debug.Log("Received image data length: " + downloadHandler.data.Length);
+        //        Debug.Log("아바타 이미지가 성공적으로 UI에 적용되었습니다.");
+        //    }
+        //};
+
+        //yield return StartCoroutine(HttpManager.GetInstance().UploadFileByFormDataArt(info, pngData));
     }
 
     // 캐릭터 생성하기 버튼 누르면 서버에 전송
@@ -73,41 +139,6 @@ public class K_HttpAvatar : MonoBehaviour
         StartCoroutine(UploadTextureAsPng());
     }
 
-    // 서버에서 아바타 받아오기
-    public IEnumerator OnDownloadImage()
-    {
-        HttpInfo info = new HttpInfo
-        {
-            url = downloadUrl,
-            onComplete = (DownloadHandler downloadHandler) =>
-            {
-                Texture2D receivedTexture = new Texture2D(2, 2);
-                receivedTexture.LoadImage(downloadHandler.data);
-                rawImage.texture = receivedTexture;
-
-                Sprite receivedSprite = Sprite.Create(
-                    receivedTexture,
-                    new Rect(0, 0, receivedTexture.width, receivedTexture.height),
-                    new Vector2(0.5f, 0.5f)
-                    );
-                avatarImage.sprite = receivedSprite;
-
-                Debug.Log("아바타 이미지 다운로드 댐");
-
-                // 응답 받아
-                //Debug.Log("Download 완료 : " + downloadHandler.text);
-            }
-        };
-        
-        yield return StartCoroutine(HttpManager.GetInstance().Get(info));
-
-    }
-
-    public void DownloadAvatar()
-    {
-        StartCoroutine(OnDownloadImage());
-    }
-
 
     // 아바타 관련 데이터
     [System.Serializable]
@@ -115,9 +146,16 @@ public class K_HttpAvatar : MonoBehaviour
     {
         public int userId;
         public string avatarImg;
+    }
+
+    // 아바타 관련 데이터
+    [System.Serializable]
+    public struct UserAnimationData
+    {
         public string animation_idle;
         public string animation_walk;
     }
+
 
     [System.Serializable]
     public struct UserAvatarDataArray
