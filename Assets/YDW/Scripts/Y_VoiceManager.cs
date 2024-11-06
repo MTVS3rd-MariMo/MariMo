849b0a9aa8c6a7f389ff8689c17eb6aeb1423fc9
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-11-06 09:31:51 +0900

    voicetest 1

--- Assets/YDW/Scripts/Y_VoiceManager.cs
+++ Assets/YDW/Scripts/Y_VoiceManager.cs
@@ -136,8 +136,8 @@ public class Y_VoiceManager : MonoBehaviourPun
                 
                 //Debug.Log($"녹음 Dictionary 에 저장됨: {testInt}");
 
-                SaveAsWav(trimmedRecording, "C:\\Users\\Admin\\OneDrive\\문서\\FinalProject\\HotSeatingAudio\\" + testInt + filename + ".wav");
-                Debug.Log($"Wav 파일로 저장됨: {testInt}");
+                //SaveAsWav(trimmedRecording, "C:\\Users\\Admin\\OneDrive\\문서\\FinalProject\\HotSeatingAudio\\" + testInt + filename + ".wav");
+                //Debug.Log($"Wav 파일로 저장됨: {testInt}");
             }
 
             RPC_UpdateTestInt();
