2a92f414e4a6fca892c3106f949ac7a01a892dcc
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-11-07 14:53:35 +0900

    로그인/맵셋업 http 통신

--- Assets/YDW/Scripts/Server/Y_HttpHotSeat.cs
+++ Assets/YDW/Scripts/Server/Y_HttpHotSeat.cs
@@ -1,6 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public class SelfIntroduce
+{
+    public int userId;
+    public int lessonId;
+    public string selfIntroduce;
+}
+
+[Serializable]
+public class InterviewFile
+{
+    public int lessonId;
+    public string userName;
+    public string character;
+    public byte[] wavFile;
+}
 
 public class Y_HttpHotSeat : MonoBehaviour
 {
@@ -31,15 +50,103 @@ public class Y_HttpHotSeat : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public IEnumerator Put(HttpInfo info)
     {
-        
+        using (UnityWebRequest webRequest = new UnityWebRequest(info.url, "PUT"))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(info.body);
+            webRequest.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+            webRequest.SetRequestHeader("userId", Y_HttpLogIn.GetInstance().userId.ToString());
+
+            // 서버에 요청 보내기
+            yield return webRequest.SendWebRequest();
+
+            // 서버에게 응답이 왔다.
+            DoneRequest(webRequest, info);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void DoneRequest(UnityWebRequest webRequest, HttpInfo info)
     {
-        
+        // 만약에 결과가 정상이라면
+        if (webRequest.result == UnityWebRequest.Result.Success)
+        {
+            // 응답 온 데이터를 요청한 클래스로 보내자
+            if (info.onComplete != null)
+            {
+                info.onComplete(webRequest.downloadHandler);
+            }
+        }
+        // 그렇지 않다면 (Error 라면)
+        else
+        {
+            // Error 의 이유를 출력
+            Debug.LogError("Net Error : " + webRequest.error);
+        }
     }
+
+
+    // 핫시팅 자기소개 전송
+    public string sendSelfIntroduceUrl = "api/lesson-result/hot-sitting/self-inroduce";
+
+    public IEnumerator SendSelfIntroduce(string content)
+    {
+        SelfIntroduce selfIntroduce = new SelfIntroduce
+        {
+            userId = Int32.Parse(Y_HttpLogIn.GetInstance().userId),
+            lessonId = 101, // 더미!!!!!
+            selfIntroduce = "안녕하세요"
+        };
+
+        // JSON 형식으로 변환
+        string jsonBody = JsonUtility.ToJson(selfIntroduce);
+
+        // HttpInfo 설정
+        HttpInfo info = new HttpInfo
+        {
+            url = Y_HttpLogIn.GetInstance().mainServer + sendSelfIntroduceUrl,
+            body = jsonBody,
+            contentType = "application/json",
+            onComplete = (DownloadHandler downloadHandler) =>
+            {
+                Debug.Log("자기소개 보내기 성공: " + downloadHandler.text);
+            }
+        };
+
+        // Put 메서드 호출
+        yield return StartCoroutine(Put(info));
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            GetSelfIntroduce();
+        }
+    }
+
+    string mySelfIntroduce;
+
+    void GetSelfIntroduce()
+    {
+        mySelfIntroduce = Y_HotSeatController.Instance.selfIntroduceInput.text;
+        print("자기소개예요 : " + mySelfIntroduce);
+    }
+
+    // 핫시팅 음성파일 전송
+    public string sendInterviewWAV = "api/lesson-result/hot-sitting/record";
+
+    //public IEnumerator SendInterviewFile(byte[] record)
+    //{
+    //    InterviewFile interviewFile = new InterviewFile
+    //    {
+    //        lessonId = 101,
+    //        userName = "정현민",
+    //        character = "바다거북",
+    //        wavFile = new byte[]
+    //    };
+    //}
+
 }
--- Assets/YDW/Scripts/Server/Y_HttpRoomSetUp.cs
+++ Assets/YDW/Scripts/Server/Y_HttpRoomSetUp.cs
@@ -22,11 +22,24 @@ public class UserIdList
     public List<int> userIds;
 }
 
+[Serializable]
+public class ClassMaterial
+{
+    public string bookTitle;
+    public string bookContents;
+    public List<Quiz> quizzes;
+    public List<OpenQuestion> openQuestions;
+    public List<string> lessonRoles;
+}
+
 public class Y_HttpRoomSetUp : MonoBehaviour
 {
     static Y_HttpRoomSetUp instance;
 
-    List<int> userList = new List<int>();
+    public List<int> userList = new List<int>();
+
+    public ClassMaterial realClassMaterial;
+
 
     public static Y_HttpRoomSetUp GetInstance()
     {
@@ -116,54 +129,42 @@ public class Y_HttpRoomSetUp : MonoBehaviour
         }
     }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Alpha8))
-    //    {
-    //        SendLessonId();
-    //        //print("userID 프린트하기 : " + Y_HttpLogIn.GetInstance().userId);
-    //    }
-    //}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            StartCoroutine(GetClassMaterial());
+            //print("userID 프린트하기 : " + Y_HttpLogIn.GetInstance().userId);
+        }
+    }
 
-    public string sendLessonIdUrl = "http://211.250.74.75:8202/api/lesson/enter";
+    public string sendLessonIdUrl = "api/lesson/enter/";
 
     public IEnumerator SendLessonId()
     {
         SendLessonId sendLessonId = new SendLessonId
         {
-            lessonId = 101 // 더미!!!!!
+            lessonId = 101
+            // 더미!!!!!
         };
 
+        // JSON 형식으로 변환
         string jsonBody = JsonUtility.ToJson(sendLessonId);
-        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonBody);
 
-        using (UnityWebRequest webRequest = new UnityWebRequest(sendLessonIdUrl, "PUT"))
+        // HttpInfo 설정
+        HttpInfo info = new HttpInfo
         {
-            webRequest.uploadHandler = new UploadHandlerRaw(jsonToSend); // 로우 데이터 업로드
-            webRequest.downloadHandler = new DownloadHandlerBuffer(); // 서버가 다운로드 할 수 있는 공간 만듦
-
-            // 서버에 요청 보내기
-            yield return webRequest.SendWebRequest();
-
-            // 서버 응답 처리
-            if (webRequest.result == UnityWebRequest.Result.Success)
+            url = Y_HttpLogIn.GetInstance().mainServer + sendLessonIdUrl + sendLessonId.lessonId.ToString(),
+            body = jsonBody,
+            contentType = "application/json",
+            onComplete = (DownloadHandler downloadHandler) =>
             {
-                Debug.Log("레슨 아이디 보내기 성공: " + webRequest.downloadHandler.text);
+                Debug.Log("레슨 아이디 보내기 성공: " + downloadHandler.text);
             }
-            else
-            {
-                Debug.LogError("레슨 아이디 보내기 실패: " + webRequest.error);
-            }
-        }
+        };
 
-        //HttpInfo info = new HttpInfo();
-        //info.url = "http://211.250.74.75:8202/api/lesson/enter";
-        //info.onComplete = (DownloadHandler downloadHandler) =>
-        //{
-        //    print("레슨 아이디 보냈습니다");
-        //};
-
-        //StartCoroutine(Put(info));
+        // Put 메서드 호출
+        yield return StartCoroutine(Put(info));
     }
 
     public IEnumerator GetUserIdList()
@@ -174,9 +175,9 @@ public class Y_HttpRoomSetUp : MonoBehaviour
         };
 
         string jsonBody = JsonUtility.ToJson(sendLessonId);
-        string requestUserListUrl = "http://211.250.74.75:8202/api/lesson/participant/" + sendLessonId.lessonId;
+        string requestUserListUrl = "api/lesson/participant/";
 
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(requestUserListUrl))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(Y_HttpLogIn.GetInstance().mainServer + requestUserListUrl + sendLessonId.lessonId.ToString()))
         {
             webRequest.downloadHandler = new DownloadHandlerBuffer(); // 서버가 다운로드 할 수 있는 공간 만듦
 
@@ -205,9 +206,60 @@ public class Y_HttpRoomSetUp : MonoBehaviour
 
     }
 
-    public void GetClassMaterial()
+    public IEnumerator GetClassMaterial()
     {
+        SendLessonId sendLessonId = new SendLessonId
+        {
+            lessonId = 101 // 더미!!!!!
+        };
 
+        string jsonBody = JsonUtility.ToJson(sendLessonId);
+        string requestUserListUrl = "api/lesson/";
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(Y_HttpLogIn.GetInstance().mainServer + requestUserListUrl + sendLessonId.lessonId.ToString()))
+        {
+            webRequest.downloadHandler = new DownloadHandlerBuffer(); // 서버가 다운로드 할 수 있는 공간 만듦
+
+            // 서버에 요청 보내기
+            yield return webRequest.SendWebRequest();
+
+            // 서버 응답 처리
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("수업자료 받아오기 성공: " + webRequest.downloadHandler.text);
+
+                // JSON 데이터 파싱
+                ClassMaterial classMaterial = JsonUtility.FromJson<ClassMaterial>(webRequest.downloadHandler.text);
+                print("bookTitle : " + classMaterial.bookTitle);
+                print("bookTContent : " + classMaterial.bookContents);
+
+                foreach(Quiz quiz in classMaterial.quizzes)
+                {
+                    print("퀴즈 질문 : " + quiz.question);
+                    print("퀴즈 답 : " + quiz.answer);
+                    print("퀴즈 선지1 : " + quiz.choices1);
+                    print("퀴즈 선지2 : " + quiz.choices2);
+                    print("퀴즈 선지3 : " + quiz.choices3);
+                    print("퀴즈 선지4 : " + quiz.choices4);
+                }
+
+                foreach(OpenQuestion openQuestion in classMaterial.openQuestions)
+                {
+                    print("열린 질문 : " + openQuestion.questionTitle);
+                }
+
+                foreach(string lessonRole in classMaterial.lessonRoles)
+                {
+                    print("레슨 롤 : " + lessonRole);
+                }
+
+                realClassMaterial = classMaterial;
+            }
+            else
+            {
+                Debug.LogError("수업자료 받아오기 실패: " + webRequest.error);
+            }
+        }
     }
 
 }
