ffbaf3f2422fa41f89ddb6e7224e86e13638b21d
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-11-17 18:22:07 +0900

    자잘한 UI 수정
    핫시팅 UI 중 화살표와 타이머 이미지 변경, 생성 중 로딩창 추가, 맵에서 캐릭터 이름표 추가

---
+++ Assets/Y_PlayerNameTag.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Y_PlayerNameTag : MonoBehaviourPun
+{
+    public Sprite myNameTag;
+    public GameObject nameUI;
+    public PhotonView pv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pv = GetComponentInParent<PhotonView>();
+        nameUI.GetComponentInChildren<TMP_Text>().text = PhotonNetwork.NickName;
+        if(pv.IsMine)
+        {
+            nameUI.GetComponent<Image>().sprite = myNameTag;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
