eea2f3f11c3160d2700ca610e22beb2f91fec200
hzkkong <hz0227798@gmail.com>
2024-11-19 20:46:22 +0900

    임시커밋3

--- Assets/Hz/Scripts/Server/K_HttpAvatar.cs
+++ Assets/Hz/Scripts/Server/K_HttpAvatar.cs
@@ -32,7 +32,7 @@ public class K_HttpAvatar : MonoBehaviourPun
 
 
     // URL
-    public string uploadUrl = "http://211.250.74.75:8202/api/avatar/upload-img";
+    public string uploadUrl = "http://211.250.74.75:8202/api/avatar/aws/upload-img";
     private string avatarImgUrl;
     private List<string> animationUrls;
 
