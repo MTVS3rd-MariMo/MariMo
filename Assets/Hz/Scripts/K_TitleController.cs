f012ccb3377df08b32abdba6a425a2e6807bfec5
hzkkong <khj0227798@gmail.com>
2024-11-23 18:43:01 +0900

    타이틀 UI 적용

--- Assets/Hz/Scripts/K_TitleController.cs
+++ Assets/Hz/Scripts/K_TitleController.cs
@@ -7,12 +7,12 @@ public class K_TitleController : MonoBehaviour
     // 스타트 타이틀
     public GameObject img_startTitle;
     // 스타트 타이틀 애니메이션
-    public Animation anim_Title;
+    //public Animation anim_Title;
 
     void Start()
     {
         // 애니메이션 컴포넌트 찾아주고
-        anim_Title = GetComponent<Animation>();
+        //anim_Title = GetComponent<Animation>();
         // 스타트 타이틀 재생 끝나면 UI 꺼버림
         //anim_Title.Play();
         StartCoroutine(UnSeenTitle());
@@ -24,7 +24,7 @@ public class K_TitleController : MonoBehaviour
         //yield return new WaitForSeconds(delay);
 
         //// 애니메이션 플레이
-        anim_Title.Play("Title_Animation");
+        //anim_Title.Play("Title_Animation");
         // 3초 후 UI 꺼주기
         yield return new WaitForSeconds(3f);
         img_startTitle.SetActive(false);
