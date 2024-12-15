70740e3a31543b36f4821741013e49b5d4291411
hzkkong <hz0227798@gmail.com>
2024-12-15 14:28:15 +0900

    아바타 엔드포인트 변경

--- Assets/Hz/Scripts/Server/K_HttpAvatar.cs
+++ Assets/Hz/Scripts/Server/K_HttpAvatar.cs
@@ -25,7 +25,7 @@ public class K_HttpAvatar : MonoBehaviourPun
     public GameObject btn_ToMap;
 
     // URL
-    public string uploadUrl = "http://211.250.74.75:8202/api/avatar/upload-img";
+    public string uploadUrl = "http://211.250.74.75:8202/api/avatar/dummy/upload-img";
     private string avatarImgUrl;
     private List<string> animationUrls;
 
